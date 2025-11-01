692b1cd735a0b44889e02c7f2357935db53baddd
Ant <anthony.meadows2000@outlook.com>
2025-11-01 07:15:20 -0600

    Reworked stamina, reworked classes

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -4,9 +4,6 @@ extends CharacterBody2D
 @export var enable_movement: bool = true
 @onready var stamina_regen_timer: Timer = $stamina_bar_component/stamina_regen_timer
 
-@export var regen_rate_per_sec: float = 20.0 # Rate at which stamina regenerates per second
-var _regen_enabled: bool = false # Flag that determines if stamina should currently be regenerating
-
 func _ready() -> void:
 	# Makes a copy of the resource file PlayerStats 
 	# so each player can have their own unique stats instance
@@ -29,14 +26,14 @@ func _physics_process(_delta: float) -> void:
 		playerMovement(_delta)
 	
 	# Gradually regenerate stamina after cooldown period has ended
-	if _regen_enabled and player_stats.current_stamina < player_stats.max_stamina:
+	if player_stats.stam_regen_enabled and player_stats.current_stamina < player_stats.max_stamina:
 		player_stats.current_stamina = min(
 			player_stats.max_stamina,
-			player_stats.current_stamina + regen_rate_per_sec * _delta
+			player_stats.current_stamina + player_stats.stam_regen_rate_per_sec * _delta
 		)
 		# Once fully regenerated, stop regen to prevent unnecessary processing
 		if player_stats.current_stamina >= player_stats.max_stamina:
-			_regen_enabled = false
+			player_stats.stam_regen_enabled = false
 
 func playerMovement(_delta: float) -> void:
 	var inputVector: Vector2 = Vector2.ZERO
@@ -66,7 +63,7 @@ func playerMovement(_delta: float) -> void:
 		player_stats.sprint(player_stats.stamina_drain_per_sec * _delta)
 
 		# Disable stamina regeneration while sprinting
-		_regen_enabled = false
+		player_stats.stam_regen_enabled = false
 
 		# Stop any regen cooldown timer currently running
 		if stamina_regen_timer.time_left > 0:
@@ -82,14 +79,13 @@ func playerMovement(_delta: float) -> void:
 		# Start regen cooldown only when stamina isn't full and not already regenerating
 		if player_stats.current_stamina < player_stats.max_stamina \
 		and stamina_regen_timer.time_left <= 0 \
-		and not _regen_enabled:
+		and not player_stats.stam_regen_enabled:
 			stamina_regen_timer.start()  # Starts one-shot cooldown before regen begins
 
 	# Use move_and_slide for smooth top-down character movement
 	move_and_slide()
 
-
 # Called when the stamina regen timer finishes its cooldown delay
 func _on_stamina_regen_timer_timeout() -> void:
 	# Enables stamina regeneration logic in _physics_process
-	_regen_enabled = true
+	player_stats.stam_regen_enabled = true
