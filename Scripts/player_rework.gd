5b817827a66c47c2194f8997def4c37357e86b5b
Ant <anthony.meadows2000@outlook.com>
2025-11-04 15:41:12 -0700

    reworked player movement

--- Scripts/player_rework.gd
+++ Scripts/player_rework.gd
@@ -9,23 +9,15 @@ func _physics_process(_delta: float) -> void:
 		playerMovement(_delta)
 
 func playerMovement(_delta: float) -> void:
-	var inputVector: Vector2 = Vector2.ZERO
-
-	# Read WASD input
-	if Input.is_action_pressed("move_right"):
-		inputVector.x += 1
-	if Input.is_action_pressed("move_left"):
-		inputVector.x -= 1
-	if Input.is_action_pressed("move_down"):
-		inputVector.y += 1
-	if Input.is_action_pressed("move_up"):
-		inputVector.y -= 1
-
+	## Read WASD input
+	var inputVector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * 300 #300 is player speed for now
+	
 	# Normalize so diagonals aren't faster
 	if inputVector != Vector2.ZERO:
 		inputVector = inputVector.normalized()
 
-	velocity = inputVector * 300
 
 	# Use move_and_slide for smooth top-down character movement
 	move_and_slide()
