f276a7d7343a972f581f6dd031f1e7c8c2aa188f
Ant <anthony.meadows2000@outlook.com>
2025-11-08 04:03:37 -0700

    interactable label

--- main/Scripts/pong_game.gd
+++ main/Scripts/pong_game.gd
@@ -1,14 +1,20 @@
-# PongMachine.gd (on the machine node in your world)
 extends Node2D
+class_name PongGame
 
 @onready var playable_comp := $PlayableGameComponent
+@onready var game_label := $game_label
+
 var player_nearby := false
 
 func _on_player_detector_area_entered(a: Area2D) -> void:
-	if a.is_in_group("player"): player_nearby = true
+	if a.is_in_group("player"):
+		player_nearby = true
+		game_label.show()
 
 func _on_player_detector_area_exited(a: Area2D) -> void:
-	if a.is_in_group("player"): player_nearby = false
+	if a.is_in_group("player"):
+		player_nearby = false
+		game_label.hide()
 
 func _process(_d: float) -> void:
 	if player_nearby and Input.is_action_just_pressed("interact"):
